e2541993e029a0bbd614a5ad1053df74865154f3
Guilhem Allaman <guilhemallaman@hotmail.fr>
2019-12-09 10:18:44 +0100

    convert Fml in CNF format to CNFFml object

--- src/Data/Algorithm/Sat/Query.hs
+++ src/Data/Algorithm/Sat/Query.hs
@@ -6,16 +6,16 @@ module Data.Algorithm.Sat.Query
     tautology
   ) where
 
+import Data.Maybe
 import qualified Data.Algorithm.Sat.Fml as Fml
 import qualified Data.Algorithm.Sat.Assignment as Assignment
+import qualified Data.Algorithm.Sat.Solver as Solver
 
 satisfiable :: (Ord a) => Fml.Fml a -> Bool
--- TODO
-satisfiable _ = True
+satisfiable a = isJust (Solver.solve a)
 
 satisfyingAssignment :: (Ord a) => Fml.Fml a -> Maybe (Assignment.Assignment a)
--- TODO
-satisfyingAssignment _ = Nothing
+satisfyingAssignment a = Solver.solve a
 
 satisfyingAssignments :: (Ord a) => Fml.Fml a -> [Assignment.Assignment a]
 -- TODO
--- src/Data/Algorithm/Sat/Solver/CNFFml.hs
+++ src/Data/Algorithm/Sat/Solver/CNFFml.hs
@@ -1,8 +1,23 @@
 module Data.Algorithm.Sat.Solver.CNFFml
   (
-    CNFFml(..)
+    CNFFml(..),
+    fmlToCNFFml
   ) where
 
-import qualified Data.Algorithm.Sat.Solver.Clause as Solver.Clause
+import qualified Data.Algorithm.Sat.Fml as Fml
+import qualified Data.Algorithm.Sat.Lit as Lit
+import qualified Data.Algorithm.Sat.Solver.Clause as Clause
 
-newtype CNFFml a = CNFFml { getClauses :: [Solver.Clause.Clause a] }
+newtype CNFFml a = CNFFml { getClauses :: [Clause.Clause a] }
+
+toLit :: Fml.Fml a -> Lit.Lit a
+toLit (Fml.Final a) = Lit.T a
+toLit (Fml.Not (Fml.Final a)) = Lit.F a
+
+toClause :: Fml.Fml a -> Clause.Clause a
+toClause (Fml.Or a b) = Clause.Clause (Clause.getLits (toClause a) ++ Clause.getLits (toClause b))
+toClause a = Clause.Clause [toLit a]
+
+fmlToCNFFml :: Fml.Fml a -> CNFFml a
+fmlToCNFFml (Fml.And a b) = CNFFml (getClauses (fmlToCNFFml a) ++ getClauses (fmlToCNFFml b))
+fmlToCNFFml a = CNFFml [toClause a]
