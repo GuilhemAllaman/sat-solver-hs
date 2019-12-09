e6ad38bfc7187efe26be02409e6dbb338147d760
Guilhem Allaman <guilhemallaman@hotmail.fr>
2019-12-09 11:29:44 +0100

    nice printing to check and debug

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,20 @@ module Main where
 
 import qualified Data.Algorithm.Sat.Fml.Examples as Ex
 import qualified Data.Algorithm.Sat.Fml as Fml
+import qualified Data.Algorithm.Sat.Solver.CNFFml as CNFFml
+
+fmlPrinter :: Show a => Fml.Fml a -> String
+fmlPrinter a = "Raw :\n"
+  ++ (Fml.prettyPrinter a)
+  ++ "\nToCNF :\n"
+  ++ (Fml.prettyPrinter . Fml.toCNF $ a)
+  ++ "\nClauses :\n"
+  ++ (show . CNFFml.getClauses . CNFFml.fmlToCNFFml . Fml.toCNF $ a)
+  ++ "\n"
 
 main :: IO ()
-main = putStrLn (Fml.prettyPrinter Ex.fml1)
-  >> putStrLn (show (Fml.vars Ex.fml1))
-  >> putStrLn (Fml.prettyPrinter Ex.fml2)
-  >> putStrLn (show (Fml.vars Ex.fml2))
-  >> putStrLn (Fml.prettyPrinter Ex.fml3)
-  >> putStrLn (show (Fml.vars Ex.fml3))
+main = putStrLn ("fml1") >> putStrLn (fmlPrinter Ex.fml1)
+  >> putStrLn ("fml2") >> putStrLn (fmlPrinter Ex.fml2)
+  >> putStrLn ("fml3") >> putStrLn (fmlPrinter Ex.fml3)
+  >> putStrLn ("fml4") >> putStrLn (fmlPrinter Ex.fml4)
+  >> putStrLn ("fml5") >> putStrLn (fmlPrinter Ex.fml5)
